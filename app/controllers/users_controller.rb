39ca6fa793500a1d42aa7f094fec33831549edcc
Alexander Schuessler <schuessler.alex@gmail.com>
2022-06-08 11:05:42 -0500

    commit tree 2a7470c32b7ec826b3e85d699ab2f4520f5e1dca

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  private
+
+  def user_not_found
+    render json: {error: 404}, status: :not_found
   end
 
 end
